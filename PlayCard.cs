6aa8b63c1301ba4d3fc9698748a2d6e3329c3df0
Chander Parkash <cpmourya@gmail.com>
2025-08-10 13:57:24 +0530

    Summary added

--- PlayCard.cs
+++ PlayCard.cs
@@ -1,4 +1,7 @@
-﻿public class PlayCard
+﻿/// <summary>
+/// This class represents a card that can be played in a game.
+/// </summary>
+public class PlayCard
 {
     public string CardFaceValue { get; set; }
     public string CardType { get; set; }
